ae79c8c684fec4ebb1bb350737051d6a2d285896
followize_bruno <bruno@followize.com.br>
2021-03-29 13:25:24 -0300

    feat: inclusao/ajustes de logicas em templates
    Resolve:[master]

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from './../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Array<Recipe> = [
     new Recipe(
       'Test Recipe',
@@ -14,7 +15,7 @@ export class RecipeListComponent implements OnInit {
       'https://s2.glbimg.com/jOvVw-5MaZswJgZvZ8ya1V7ZMXE=/0x0:800x533/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2018/c/r/mLrOqnRiemx034biSdHw/ratatouille.jpg'
     ),
     new Recipe(
-      'Test Recipe',
+      'Another Recipe',
       'Description Recipe',
       'https://s2.glbimg.com/jOvVw-5MaZswJgZvZ8ya1V7ZMXE=/0x0:800x533/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2018/c/r/mLrOqnRiemx034biSdHw/ratatouille.jpg'
     ),
@@ -23,4 +24,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
